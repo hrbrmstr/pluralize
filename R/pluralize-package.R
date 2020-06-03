90bd18bc6e7f69921a383c265b02134f1042b4ad
hrbrmstr <bob@rud.is>
2020-06-03 08:44:26 -0400

    DESCRIPTION / CRAN prep

--- R/pluralize-package.R
+++ R/pluralize-package.R
@@ -2,7 +2,7 @@
 #'
 #'Tools are provided to create plural, singular and regular forms of
 #' English words along with tools to augment the built-in rules to fit
-#' specialzied needs. Core functionality is based on a JavaScript library,
+#' specializied needs. Core functionality is based on a JavaScript library,
 #' <https://github.com/blakeembrey/pluralize>, by Blake Embrey.
 #'
 #' @name pluralize
